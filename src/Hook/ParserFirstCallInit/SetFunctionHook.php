6b60ceb313bee64056b265cb1a44f7b2add73e1e
Dejan Savuljesku <d.savuljesku@gmail.com>
2020-02-13 12:21:13 +0100

    Modernise extension

---
+++ src/Hook/ParserFirstCallInit/SetFunctionHook.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace MediaWiki\Extension\DrawioEditor\Hook\ParserFirstCallInit;
+
+use MediaWiki\Extension\DrawioEditor\DrawioEditor;
+use MWException;
+use Parser;
+
+class SetFunctionHook {
+
+	/**
+	 * @param Parser $parser
+	 * @return bool
+	 * @throws MWException
+	 */
+	public static function callback( &$parser ) {
+		$drawioEditor = new DrawioEditor();
+		$parser->setFunctionHook( 'drawio', [ $drawioEditor, 'parse' ] );
+
+		return true;
+	}
+}
