2f084694978633eaf65b5070a8400a62ea301943
Wout Gevaert <wout@wikibase.nl>
2023-03-30 12:42:09 +0200

    Improve PNG graphmodel handling
    This patch allows for extracting mxfile data from png
    when it is stored in zTXt instead of in tEXt.
    
    Complies with https://w3c.github.io/PNG-spec/
    
    Change-Id: Iaa20f978f44f15bcf6798ba12f51a6f906c48504

--- src/MXDocumentExtractor/PNG.php
+++ src/MXDocumentExtractor/PNG.php
@@ -2,6 +2,8 @@
 
 namespace MediaWiki\Extension\DrawioEditor\MXDocumentExtractor;
 
+use Wikimedia\AtEase\AtEase;
+
 class PNG extends Base {
 
 	/**
@@ -16,7 +18,36 @@ class PNG extends Base {
 		$partiallyDecodedXML = urldecode( $encodedXML );
 		$matches = [];
 		preg_match( '#<mxfile.*?>(.*?)</mxfile>#s', $partiallyDecodedXML, $matches );
-		$strippedXML = $matches[0] ?? '';
+		if ( empty( $matches ) ) {
+			// Extract from zTXt instead of tEXt.
+			$type = 'zTXt';
+			// The 2 zero bytes are for null-termination of comment (one byte) and encoding type (another byte).
+			// See https://w3c.github.io/PNG-spec/#11zTXt
+			$comment = 'mxGraphModel' . "\x0\x0";
+			$position = strpos( $this->imageContent, $type . $comment );
+			if ( $position !== false ) {
+				// 4 bytes before chunk type give chunck length.
+				// See https://w3c.github.io/PNG-spec/#5Chunk-layout
+				$unpacked = unpack( "N", substr( $this->imageContent, $position - 4, 4 ) );
+				$chunckLength = $unpacked["1"];
+				$data = substr(
+					$this->imageContent,
+					$position + strlen( $type . $comment ),
+					$chunckLength - strlen( $comment )
+				);
+				AtEase::suppressWarnings();
+				$data = gzinflate( $data );
+				AtEase::restoreWarnings();
+				if ( !is_string( $data ) ) {
+					$data = '';
+				}
+				$strippedXML = urldecode( $data );
+			} else {
+				$strippedXML = '';
+			}
+		} else {
+			$strippedXML = $matches[0];
+		}
 		return trim( $strippedXML );
 	}
 }
