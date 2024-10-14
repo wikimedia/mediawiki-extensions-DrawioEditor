9468d04a39adc102d845b86985ac8ac4bc511b06
hslater <hamish.slater@hallowelt.com>
2024-10-14 19:55:26 +1300

    Enable custom alt text for Drawio images
    Maintain consistent filename in inspector
    
    ERM38914
    
    [REL1_39, master]
    
    Change-Id: Ic02ff7af34703e92ff56797ded6b78d788cad62e

--- resources/ve/ve.ui.DrawioInspector.js
+++ resources/ve/ve.ui.DrawioInspector.js
@@ -22,7 +22,7 @@ ve.ui.DrawioInspector.static.selfCloseEmptyBody = false;
 ve.ui.DrawioInspector.prototype.initialize = function () {
 	ve.ui.DrawioInspector.super.prototype.initialize.call( this );
 
-	var filenameProcessor = { processor: new drawioeditor.FilenameProcessor() };
+	const filenameProcessor = { processor: new drawioeditor.FilenameProcessor() }; // eslint-disable-line no-undef
 	mw.hook( 'drawioeditor.makeFilenameProcessor' ).fire( filenameProcessor );
 	this.filenameProcessor = filenameProcessor.processor;
 
@@ -38,7 +38,7 @@ ve.ui.DrawioInspector.prototype.initialize = function () {
 	);
 };
 
-ve.ui.DrawioInspector.prototype.createLayout = function ( ) {
+ve.ui.DrawioInspector.prototype.createLayout = function () {
 	this.indexLayout = new OO.ui.PanelLayout( {
 		expanded: false,
 		padded: true
@@ -51,39 +51,58 @@ ve.ui.DrawioInspector.prototype.createLayout = function ( ) {
 	this.fileNameInputWidget.on( 'change', this.onFileNameChange, [], this );
 	this.fileNameInputLayout = new OO.ui.FieldLayout( this.fileNameInputWidget, {
 		align: 'left',
-		label: OO.ui.deferMsg( 'drawioconnector-ve-drawio-tag-name' )
+		label: OO.ui.deferMsg( 'drawioconnector-ve-drawio-tag-name' ),
+		help: OO.ui.deferMsg( 'drawioconnector-ve-drawio-tag-name-help' )
+	} );
+
+	// InputWidget for Alt Text
+	this.altTextInputWidget = new OO.ui.TextInputWidget();
+	this.altTextInputLayout = new OO.ui.FieldLayout( this.altTextInputWidget, {
+		align: 'left',
+		label: OO.ui.deferMsg( 'drawioconnector-ve-drawio-alt-label' ),
+		help: OO.ui.deferMsg( 'drawioconnector-ve-drawio-alt-help' )
 	} );
 
 	// set default values
 	this.fileNameInputWidget.setValue( this.filename );
 
 	this.indexLayout.$element.append(
-		this.fileNameInputLayout.$element
+		this.fileNameInputLayout.$element,
+		this.altTextInputLayout.$element
 	);
 };
 
 ve.ui.DrawioInspector.prototype.onFileNameChange = function () {
-	var actions = this.actions;
+	const actions = this.actions;
 	actions.setAbilities( { done: false } );
-	this.fileNameInputWidget.getValidity().done( function() {
+	this.fileNameInputWidget.getValidity().done( function () {
 		actions.setAbilities( { done: true } );
 	} );
 };
 
 ve.ui.DrawioInspector.prototype.getSetupProcess = function ( data ) {
 	return ve.ui.DrawioInspector.super.prototype.getSetupProcess.call( this, data )
-		.next( function () {
-			this.selectedNode.getAttribute( 'mw' ).attrs;
+		.next( () => {
+			const attributes = this.selectedNode.getAttribute( 'mw' ).attrs;
+			if ( attributes.filename ) {
+				this.fileNameInputWidget.setValue( attributes.filename );
+			}
+			if ( attributes.alt ) {
+				this.altTextInputWidget.setValue( attributes.alt );
+			}
 			this.actions.setAbilities( { done: true } );
-		}, this );
+		} );
 };
 
 ve.ui.DrawioInspector.prototype.updateMwData = function ( mwData ) {
 	ve.ui.DrawioInspector.super.prototype.updateMwData.call( this, mwData );
 
-	var filename = this.fileNameInputWidget.getValue();
+	const filename = this.fileNameInputWidget.getValue();
+	const altText = this.altTextInputWidget.getValue();
+
 	// Get rid of the symbols which should not be in the filename
 	mwData.attrs.filename = this.filenameProcessor.sanitizeFilename( filename );
+	mwData.attrs.alt = altText;
 };
 
 /* Registration */
