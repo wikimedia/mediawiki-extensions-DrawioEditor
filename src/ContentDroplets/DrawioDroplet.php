80d7e01dbc713d37eed91dcb1317d6e58e115c7a
hslater <hamish.slater@hallowelt.com>
2025-01-22 13:45:27 +0100

    Update Message namespace
    ERM38444
    
    [REL1_43, master]

--- src/ContentDroplets/DrawioDroplet.php
+++ src/ContentDroplets/DrawioDroplet.php
@@ -5,7 +5,7 @@ declare( strict_types = 1 );
 namespace MediaWiki\Extension\DrawioEditor\ContentDroplets;
 
 use MediaWiki\Extension\ContentDroplets\Droplet\TagDroplet;
-use Message;
+use MediaWiki\Message\Message;
 
 class DrawioDroplet extends TagDroplet {
 
