58fb051b4c7d94676c4fc35d9842087af5cc79eb
driedmueller <daniel.riedmueller@hallowelt.com>
2025-10-20 12:20:56 +0200

    Fix missing i18n and display mode config not evaluated
    ERM44547
    [5.1.x]
    
    Change-Id: I8b5245bdd481addceb784f8aa6c3e66309c42c30

--- src/Hook/RegisterTags.php
+++ src/Hook/RegisterTags.php
@@ -2,15 +2,23 @@
 
 namespace MediaWiki\Extension\DrawioEditor\Hook;
 
+use MediaWiki\Config\Config;
 use MediaWiki\Extension\DrawioEditor\Tag\DrawioTag;
 use MWStake\MediaWiki\Component\GenericTagHandler\Hook\MWStakeGenericTagHandlerInitTagsHook;
 
 class RegisterTags implements MWStakeGenericTagHandlerInitTagsHook {
 
 	/**
+	 * @param Config $config
+	 */
+	public function __construct( private readonly Config $config ) {
+	}
+
+	/**
 	 * @inheritDoc
 	 */
 	public function onMWStakeGenericTagHandlerInitTags( array &$tags ): void {
-		$tags[] = new DrawioTag();
+		$defaultEditMode = $this->config->get( 'DrawioEditorDefaultEditmode' );
+		$tags[] = new DrawioTag( $defaultEditMode );
 	}
 }
